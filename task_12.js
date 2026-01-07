15bd6239eda54c290d058718c6f7047454dfb667
NurgalymSabit <sabitovnurgalym211@gmail.com>
2026-01-07 13:05:16 +0500

    Change return value from -1 to undefined

--- task_12.js
+++ task_12.js
@@ -2,6 +2,6 @@ function manualFindIndex(arr, value) {
     for (let i = 0; i < arr.length; i++) {
         if (arr[i] === value) return i;
     }
-    return -1;
+    return undefined;
 }
 console.log(manualFindIndex([1,2,3],2))
